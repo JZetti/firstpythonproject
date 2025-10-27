957dba08c3545e347d641364f12d970e967d7d1f
jzettle29 <jzettle29@my.whitworth.edu>
2025-10-27 13:53:33 -0700

    Refactor main.py to enhance greeting messages and add army age calculation

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('hello world')
+
+#Greet the user
+print("Hello World")
+print("I'm glad you're here")
+print("justkidding")
+print("math operations inbound")
+
+year =1890
+_year =1993
+yearArmy = 1775
+
+# calculate army age 
+year = 2025
+yearArmy = 1775
+ageOfArmy = year - yearArmy
+print("The Army is", ageOfArmy, "Years old") 
+
+#new variables
+
+x = .10
+j = .15
+i = .20
+z = .25
+total = 47.56
+tip = total * x, total * j, total * i, total * z
+print("tip tiers are", tip)
